190c9c228ceba468bccbbcd0fc660d7ea72e3fb3
zvamc9 <99474223+zvamc9@users.noreply.github.com>
2022-09-20 18:09:57 +0300

    Update задание5.js

--- module_5/задание5.js
+++ module_5/задание5.js
@@ -1,3 +1,10 @@
-let arr = ["Vova", "2022", "Tanya", "february"];
- console.log(arr.length);
-  ["Vova", "2022", "Tanya", "february"].forEach(alert);
+//Задание 5.
+
+//Дан произвольный массив. Необходимо вывести количество элементов массива, затем перебрать его и вывести в консоль каждый элемент массива.
+
+const arr = [0, null, NaN, 0, 1, 2, 4, "str"]
+  console.log('количество элементов: ', arr.length)
+
+for (let i = 0; i < arr.length; i++) {
+  console.log(`элемент arr[${i}] = ${arr[i]}`)
+}
