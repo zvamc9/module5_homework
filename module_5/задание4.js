e44b26ad520e15ae6313d1f7f51444358de04865
zvamc9 <99474223+zvamc9@users.noreply.github.com>
2022-09-20 18:21:31 +0300

    Update задание4.js

--- module_5/задание4.js
+++ module_5/задание4.js
@@ -1,3 +1,7 @@
+//Задание 4.
+
+//Записать в переменную случайное целое число в диапазоне [0; 100]. Используйте объект Math.
+
 function  getRandomRange(min, max) {
  return Math.floor(Math.random() * (max-min));
 }
