94e8b25fa0dd1149e6a89f6b6ca0c254d0e58a85
zvamc9 <99474223+zvamc9@users.noreply.github.com>
2022-09-20 18:18:05 +0300

    Update задание8.js

--- module_5/задание8.js
+++ module_5/задание8.js
@@ -1,11 +1,15 @@
-let animals = new Map([
-    ["dog", "good"],
-    ["cat", "bad"],
-    ["parrot", "kind"]
-]);
-animals.forEach((key) => {
-    console.log(`${key} - x`);
-});
-animals.forEach((value) => {
-    console.log(` ${value} - y`);
-});
+//Задание 8.
+
+//Создайте произвольный массив Map. Получите его ключи и выведите в консоль все значения в виде «Ключ — Х, значение — Y».
+
+//Используйте шаблонные строки.
+
+let map = new Map();
+map.set('key1', 'prop1');
+map.set('key2', 'prop2');
+
+const keys = map.keys()
+
+for (let key of keys) {
+console.log(`Ключ - ${key}, значение - ${map.get(key)}`)
+}
