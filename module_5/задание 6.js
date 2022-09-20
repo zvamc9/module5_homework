e4a448e77cecbe29dfbef0ca4a0e33bc1d07677d
zvamc9 <99474223+zvamc9@users.noreply.github.com>
2022-09-20 18:07:21 +0300

    Update задание 6.js

--- module_5/задание 6.js
+++ module_5/задание 6.js
@@ -1,3 +1,12 @@
-[1,1,1,1,1].every((item,index,arr)=>{
-    return item == arr[0]
-    })
+const arr = [1, 2, 3, 3]
+
+  let isEqual = true;
+const reference = arr[0];
+
+for (let item of arr) {
+  if (item !== reference) {
+isEqual = false;
+}
+}
+
+console.log(isEqual)
